8e5bf90b348e696a33c22e4a0254515e27a19ecd
Konstantin <name.gku@gmail.com>
2022-02-01 14:39:44 +0300

    Add logger

--- nestjs-app/src/app/app.module.ts
+++ nestjs-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,12 +9,41 @@ import { UsersModule } from '../users/users.module';
 import { BoardsModule } from '../boards/boards.module';
 import { TasksModule } from '../tasks/tasks.module';
 import { getTypeOrmConfig } from '../common/app.dbconfig';
+import { LOG_LEVEL } from '../common/app.config';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env '
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: LOG_LEVEL,
+        serializers: {
+          res(reply) {
+            return {
+              statusCode: reply.statusCode,
+            }
+          },
+          req(request) {
+            return {
+              method: request.method,
+              url: request.url,
+              params: request.params,
+            };
+          }
+        },
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+            ignore: 'pid,hostname,reqId,responseTime',
+            singleLine: true,
+          }
+        },
+      }
     }),
     TypeOrmModule.forRoot(getTypeOrmConfig()),
     UsersModule,
--- nestjs-app/src/main.ts
+++ nestjs-app/src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { ClassSerializerInterceptor } from '@nestjs/common';
 import { AppModule } from './app/app.module';
+import { Logger } from 'nestjs-pino';
 
 import { PORT } from './common/app.config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(
     app.get(Reflector))
   );
+
   await app.listen(PORT, () => console.log(`Starting server on port = ${PORT} 🚀 `));
 }
 bootstrap();
